0c497a03e2cd5f0468074034d52614354e2821a3
Denis Washington <denis@denisw.de>
2023-11-12 17:23:06 +0100

    Add Atom feed

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,8 +1,10 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const rssPlugin = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   // Plugins
-  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(rssPlugin);
+  eleventyConfig.addPlugin(syntaxHighlightPlugin);
 
   // Assets
   eleventyConfig.addPassthroughCopy("images/*.jpg");
