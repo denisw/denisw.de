bfa72c358c6727b4ffbbc55e416f9869fd85ec6c
Denis Washington <denis@denisw.de>
2026-03-17 23:47:03 +0100

    Add CV page

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,7 @@
 import rssPlugin from "@11ty/eleventy-plugin-rss";
 import syntaxHighlightPlugin from "@11ty/eleventy-plugin-syntaxhighlight";
+import YAML from "yaml";
+import markdownIt from "markdown-it";
 
 export default function (eleventyConfig) {
   // Plugins
@@ -11,6 +13,15 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images/*.png");
   eleventyConfig.addPassthroughCopy("fonts/*.woff2");
   eleventyConfig.addPassthroughCopy("stylesheets/*.css");
+
+  // Data Files
+  eleventyConfig.addDataExtension("yaml", (contents) => YAML.parse(contents));
+
+  // Template Filters
+  const markdown = markdownIt({ html: true });
+  eleventyConfig.addFilter("markdown", function (value) {
+    return this.env.filters.safe(markdown.render(value));
+  });
 
   // Settings
   return {
